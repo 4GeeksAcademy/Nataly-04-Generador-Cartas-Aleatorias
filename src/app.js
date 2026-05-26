66d40f08ef4f0c2759ff58ac5aa1d6468d1f19f5
Nataly-04 <pau_nata14@hotmail.com>
2026-05-26 22:33:48 +0000

    primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+  let cardNumber = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let cardSymbols = ["♦", "♥", "♠", "♣",];
+  let randomcardNumber = [Math.floor(Math.random() * cardNumber.length)]
+  let randomcardSymbols = [Math.floor(Math.random() * cardSymbols.length)]
+
+
+  console.log(cardNumber[randomcardNumber]);
+  console.log(cardSymbols[randomcardSymbols]);
+
+  let palo_alto = document.querySelector(".palo-alto")
+  palo_alto.innerHTML = cardSymbols[randomcardSymbols]
+
+  let palo_bajo = document.querySelector(".palo-bajo")
+  palo_bajo.innerHTML = cardSymbols[randomcardSymbols]
+
+  let colorClass = cardSymbols[randomcardSymbols] === "♥" || cardSymbols[randomcardSymbols] === "♦" ? "rojo" : "negro"
+  palo_alto.classList.add(colorClass)
+  palo_bajo.classList.add(colorClass)
+
+  let numero = document.querySelector(".numero")
+  numero.innerHTML = cardNumber[randomcardNumber]
+
+
 };
+ 
